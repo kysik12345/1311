076d83559376de1798fd0cdb9d4a1311e5616ce2
kysik12345 <kysik12345@mail.ru>
2024-11-13 19:02:23 +0300

    Add files via upload
    1311

---
+++ cart/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+
+PAYMENT_CHOICES = (
+    ("cash", "Наличными"),
+    ("card_online", "Картой онлайн"),
+    ("card_courier", "Картой курьеру"),
+)
+
+DELIVERY_CHOICES = (
+    ("pick_up_point", "Пункт выдачи"),
+    ("delivery", "Курьерская доставка"),
+    ("mail", "Почта России"),
+    ("cdek", "СДЭК"),
+)
+
+
+class QuickOrderForm(forms.Form):
+    name = forms.CharField(max_length=50, label="Имя")
+    last_name = forms.CharField(max_length=50, label="Фамилия")
+    email = forms.EmailField(label="Эл.почта")
+    phone = forms.CharField(max_length=50, label="Телефон")
+    payment = forms.ChoiceField(choices=PAYMENT_CHOICES, label='Способ оплаты')
+    delivery = forms.ChoiceField(choices=DELIVERY_CHOICES, label='Способ Доставки')
